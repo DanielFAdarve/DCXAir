c16bf6577d66567a1940ba05c8104f6651609d5d
DanielFAdarve <danielfelipeavalencia@gmail.com>
2024-11-19 17:16:07 -0500

    S

---
+++ DCXAir.API/Application/Repositories/JourneyRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace DCXAir.API.Application.Repositories
+{
+    using DCXAir.API.Infrastructure;
+    using DCXAir.API.Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using DCXAir.API.Infrastructure.Data;
+    public class JourneyRepository
+    {
+        private readonly JourneyDbContext _context;
+
+        public JourneyRepository(JourneyDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Journey>> GetAllJourneysAsync()
+        {
+            return await _context.Journeys.Include(j => j.Fligths).ThenInclude(f => f.Transport).ToListAsync();
+        }
+        public async Task AddJourneyAsync(Journey journey)
+        {
+            await _context.Journeys.AddAsync(journey);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<List<Journey>> GetJourneysByOriginAndDestinationAsync(string origin, string destination)
+        {
+            return await _context.Journeys
+                .Where(j => j.Fligths.Any(f => f.Origin == origin && f.Destination == destination))
+                .ToListAsync();
+        }
+    }
+}
---
+++ DCXAir.API/Migrations/20241119214347_InitialCreate.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DCXAir.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Journeys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Journeys", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transport",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FlightCarrier = table.Column<string>(type: "TEXT", nullable: false),
+                    FlightNumber = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transport", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Origin = table.Column<string>(type: "TEXT", nullable: false),
+                    Destination = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    TransportId = table.Column<int>(type: "INTEGER", nullable: false),
+                    JourneyId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Flights_Journeys_JourneyId",
+                        column: x => x.JourneyId,
+                        principalTable: "Journeys",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Flights_Transport_TransportId",
+                        column: x => x.TransportId,
+                        principalTable: "Transport",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_JourneyId",
+                table: "Flights",
+                column: "JourneyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_TransportId",
+                table: "Flights",
+                column: "TransportId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+
+            migrationBuilder.DropTable(
+                name: "Journeys");
+
+            migrationBuilder.DropTable(
+                name: "Transport");
+        }
+    }
+}
